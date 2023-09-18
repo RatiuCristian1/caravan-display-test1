736553b5f61cdc704751cfcebba9b6c8df000db9
Cristian Ratiu <lafurnica2008@yahoo.com>
2023-09-18 21:59:46 +0200

    Working on the display of the home page

--- caravanDisplayTest1/src/pages/Home.jsx
+++ caravanDisplayTest1/src/pages/Home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <div className="home--container">
             <h1 className="home--welcome">Welcome to my webpage</h1>
-            <Link to="caravans">Find your caravan</Link>
+            <Link className="home--caravan--button" to="caravans">Find your caravan</Link>
         </div>
     )
 }
