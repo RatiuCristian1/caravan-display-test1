1128144fcd648e970cb1e84b85be748c25827223
Cristian Ratiu <lafurnica2008@yahoo.com>
2023-09-20 20:34:13 +0200

    Adding images to cloud firestore and display them in caravan.jsx

--- caravanDisplayTest1/src/components/Layout.jsx
+++ caravanDisplayTest1/src/components/Layout.jsx
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 
 export default function Layout() {
     return (
-        <div>
+        <div className="layout--container">
             <Header />
             <main>
                 <Outlet />
--- caravanDisplayTest1/src/pages/Home.jsx
+++ caravanDisplayTest1/src/pages/Home.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Home() {
+    const [animateButton, setAnimateButton] = useState(false);
+
+    useEffect(() => {
+        // Add the animation class after a delay (e.g., 1000 milliseconds = 1 second)
+        const timer = setTimeout(() => {
+            setAnimateButton(true);
+        }, 500);
+
+        // Clear the timer if the component unmounts before the delay
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <div className="home--container">
             <h1 className="home--welcome">Welcome to my webpage</h1>
-            <Link className="home--caravan--button" to="caravans">Find your caravan</Link>
+            <Link className={`home--caravan--button ${animateButton ? 'animate-slide-up' : ''}`} to="caravans">
+                Find your caravan
+            </Link>
         </div>
     )
 }
