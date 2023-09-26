5a9261253cb11f5d99982f6ca393b080d8d973e7
Cristian Ratiu <lafurnica2008@yahoo.com>
2023-09-26 23:11:41 +0200

    Caravan.jsx refactoring with new images and text

--- caravanDisplayTest1/src/pages/Caravans.jsx
+++ caravanDisplayTest1/src/pages/Caravans.jsx
@@ -45,22 +45,32 @@ export default function Caravans() {
   }, []); // The empty dependency array ensures this effect runs once after initial render
 
   return (
-    <div className="caravans-container">
-      <h1 className="caravans--title">Caravans</h1>
-      <div className="caravan-list">
-        {caravans.map((caravan) => (
-          <div key={caravan.id} className="caravan-card">
-            <Link to={`/caravans/${caravan.id}`} className="caravan-link">
-            <img
-              src={caravan.imageUrl}
-              alt={caravan.description}
-              className="caravan-image"
-            />
-            {/* <div className="caravan-description">{caravan.description}</div> */}
-            </Link>
-          </div>
-        ))}
+    <>
+      <div className="caravans-container">
+        <h1 className="caravans--title">Caravans</h1>
+        <div className="caravan-list">
+          {caravans.map((caravan) => (
+            <div key={caravan.id} className="caravan-card">
+              <Link to={`/caravans/${caravan.id}`} className="caravan-link">
+              <img
+                src={caravan.imageUrl}
+                alt={caravan.description}
+                className="caravan-image"
+              />
+              <div className="caravan-description">{caravan.name}</div>
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
+      <div className="caravans-container-bottom">
+        <h2 className="caravans-container-bottom-text">Welcome to the Caravan World Family !!</h2>
+      <img className="caravan-image-bottom" src="DET_Image_Header_1920x300_EN_3.jpg" alt="wtf is happening?" />
     </div>
+    <div className="caravans-family-container">
+      <h3 className="caravans-family-text">Embrace your family</h3>
+      <p className="caravans-family-text">We understand that families are incredibly diverse, as are their preferences when it comes to holidays. Whether large or small, regardless of the type and arrangement, every family is unique and incomparable – just like the world of caravanning. We therefore set ourselves the task of creating the right travel companion for every family. With our wide range of vehicles, from urban campers to luxury motorhomes, there is plenty to choose from.</p>
+    </div>
+  </>
   );
 }
